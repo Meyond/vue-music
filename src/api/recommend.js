7126b445178ec08386f85e88123aa67ae8d928df
meyond <332595512@qq.com>
2017-08-09 04:07:57 +0800

    2017-08-09 4:07

--- src/api/recommend.js
+++ src/api/recommend.js
@@ -1,3 +1,4 @@
+import axios from 'axios'
 import jsonp from 'common/js/jsonp'
 import {
   commonParams,
@@ -12,6 +13,25 @@ export function getRecommend() {
     uin: 0,
     needNewCode: 1
   })
-
   return jsonp(url, data, options)
+}
+
+export function getDiscList() {
+  const url = '/api/getDiscList'
+
+  const data = Object.assign({}, commonParams, {
+    platform: 'yqq',
+    hostUin: 0,
+    sin: 0,
+    ein: 29,
+    sortId: 5,
+    needNewCode: 1,
+    categoryId: 10000000,
+    rnd: Math.random()
+  })
+  return axios.get(url, {
+    param: data
+  }).then((res) => {
+    return Promise.resolve(res.data)
+  })
 }
--- src/base/slider/slider.vue
+++ src/base/slider/slider.vue
@@ -4,17 +4,22 @@
       <slot></slot>
     </div>
     <div class="dots">
-      <span v-for="(item, index ) in dots" :class="{active: currentPageIndex === index}" class="dot"></span>
+      <span v-for="(item, index ) in dots" :class="{active: currentPageIndex === index}" :key="index" class="dot"></span>
     </div>
   </div>
 </template>
 
 <script>
-import { addClass } from 'common/js/dom'
 import BScroll from 'better-scroll'
+import { addClass } from 'common/js/dom'
 
 export default {
-  name: 'slider',
+  data() {
+    return {
+      dots: [],
+      currentPageIndex: 0
+    }
+  },
   props: {
     loop: {
       type: Boolean,
@@ -29,13 +34,8 @@ export default {
       default: 4000
     }
   },
-  data() {
-    return {
-      dots: [],
-      currentPageIndex: 0
-    }
-  },
   mounted() {
+    // 使用nextTick或者setTimeout等待dom结构渲染完成后执行(浏览器刷新17ms/次)
     setTimeout(() => {
       this._setSliderWidth()
       this._initDots()
@@ -46,52 +46,42 @@ export default {
       }
     }, 20)
 
+    // 监听页面window.reSize事件,重新设置slider宽度
     window.addEventListener('resize', () => {
       if (!this.slider) {
         return
       }
-      this._setSliderWidth(true)
-      this.slider.refresh()
+      this._setSliderWidth()
     })
   },
-  activated() {
-    if (this.autoPlay) {
-      this._play()
-    }
-  },
-  deactivated() {
-    clearTimeout(this.timer)
-  },
-  beforeDestroy() {
-    clearTimeout(this.timer)
-  },
   methods: {
-    _setSliderWidth(isResize) {
+    _setSliderWidth() {
       this.children = this.$refs.sliderGroup.children
-
       let width = 0
       let sliderWidth = this.$refs.slider.clientWidth
-      for (let i = 0; i < this.children.length; i++) {
-        let child = this.children[i]
-        addClass(child, 'slider-item')
 
-        child.style.width = sliderWidth + 'px'
+      for (var i = 0; i < this.children.length; i++) {
+        var element = this.children[i]
+        addClass(element, 'slider-item') // 给每个子元素添加样式
+        element.style.width = sliderWidth + 'px'
         width += sliderWidth
       }
-      if (this.loop && !isResize) {
+      if (this.loop) {
         width += 2 * sliderWidth
       }
       this.$refs.sliderGroup.style.width = width + 'px'
     },
+    // slider初始化
     _initSlider() {
       this.slider = new BScroll(this.$refs.slider, {
         scrollX: true,
-        scrollY: false,
+        scrolly: false,
         momentum: false,
         snap: true,
         snapLoop: this.loop,
         snapThreshold: 0.3,
         snapSpeed: 400
+        // click: true
       })
 
       this.slider.on('scrollEnd', () => {
@@ -102,13 +92,8 @@ export default {
         this.currentPageIndex = pageIndex
 
         if (this.autoPlay) {
-          this._play()
-        }
-      })
-
-      this.slider.on('beforeScrollStart', () => {
-        if (this.autoPlay) {
           clearTimeout(this.timer)
+          this._play()
         }
       })
     },
@@ -120,10 +105,14 @@ export default {
       if (this.loop) {
         pageIndex += 1
       }
+      // 为了兼容鼠标拖动，每次滚动后清除SetTimeOut事件，重新设置
       this.timer = setTimeout(() => {
-        this.slider.goToPage(pageIndex, 0, 400)
+        this.slider.goToPage(pageIndex, 0, 300)
       }, this.interval)
     }
+  },
+  destroyed() {
+    clearTimeout(this.timer)
   }
 }
 </script>
