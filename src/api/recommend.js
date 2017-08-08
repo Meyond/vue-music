f14adc6721e5d6f30cbac27555c85e0f87d7ec6b
meyond <332595512@qq.com>
2017-08-09 04:10:29 +0800

    2017-08-09 4:10

--- src/api/recommend.js
+++ src/api/recommend.js
@@ -1,9 +1,6 @@
 import axios from 'axios'
 import jsonp from 'common/js/jsonp'
-import {
-  commonParams,
-  options
-} from './config.js'
+import { commonParams, options } from './config.js'
 
 export function getRecommend() {
   const url = 'https://c.y.qq.com/musichall/fcgi-bin/fcg_yqqhomepagerecommend.fcg'
