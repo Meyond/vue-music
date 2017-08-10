ccea578c85cb84ea111ec0d0a90658c758723818
meyond <332595512@qq.com>
2017-08-11 01:03:43 +0800

    2017-08-11 01:03

---
+++ src/base/scroll/scroll.vue
@@ -0,0 +1,64 @@
+<template>
+  <div ref="wrapper">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+import BScroll from 'better-scroll'
+
+export default {
+  props: {
+    probeType: {
+      type: Number,
+      default: 1
+    },
+    click: {
+      type: Boolean,
+      default: true
+    },
+    data: {
+      type: Array,
+      default: null
+    }
+  },
+  mounted() {
+    // 等数据获取并渲染完毕，撑开recommend-content后执行initScroll
+    setTimeout(() => {
+      this._initScroll()
+    }, 20)
+  },
+  methods: {
+    _initScroll() {
+      if (!this.$refs.wrapper) {
+        return
+      }
+      this.scroll = new BScroll(this.$refs.wrapper, {
+        probeType: this.probeType,
+        click: this.click
+      })
+    },
+    enable() {
+      this.scroll && this.scroll.enable()
+    },
+    disable() {
+      this.scroll && this.scroll.disable()
+    },
+    refresh() {
+      this.scroll && this.scroll.refresh()
+    }
+  },
+  watch: {
+    // data变化，刷新scroll
+    data() {
+      setTimeout(() => {
+        this.refresh()
+      }, 20)
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+
+</style>
--- src/components/recommend/recommend.vue
+++ src/components/recommend/recommend.vue
@@ -1,38 +1,54 @@
 <template>
   <div class="recommend">
-    <div class="recommend-content">
-      <div v-if="recommends.length" class="slider-wrapper">
-        <slider>
-          <div v-for="item in recommends" :key="item.id">
-            <a :href="item.linkUrl">
-              <img :src="item.picUrl" alt="banner">
-            </a>
-          </div>
-        </slider>
+    <scroll ref="scroll" :data="discList" class="recommend-content">
+      <div>
+        <div v-if="recommends.length" class="slider-wrapper">
+          <slider>
+            <div v-for="item in recommends" :key="item.id">
+              <a :href="item.linkUrl">
+                <img @load="loadImage" :src="item.picUrl" alt="banner">
+              </a>
+            </div>
+          </slider>
+        </div>
+        <div class="recommend-list">
+          <div class="list-title">热门歌单推荐</div>
+          <ul>
+            <li class="item" v-for="(item, index) in discList" :key="index">
+              <div class="icon">
+                <img :src="item.imgurl" width="60" height="60">
+              </div>
+              <div class="text">
+                <h2 class="name" v-html="item.creator.name"></h2>
+                <p class="desc" v-html="item.dissname"></p>
+              </div>
+            </li>
+          </ul>
+        </div>
       </div>
-      <div class="recommend-list">
-        <div class="list-title">热门歌单推荐</div>
-        <ul>
-        </ul>
-      </div>
-    </div>
+    </scroll>
   </div>
 </template>
 
 <script>
 import Slider from 'base/slider/slider'
+import Scroll from 'base/scroll/scroll'
 import { getRecommend, getDiscList } from 'api/recommend'
 import { ERR_OK } from 'api/config'
 
 export default {
   data() {
     return {
-      recommends: []
+      recommends: [],
+      discList: [],
+      checkLoaded: ''
     }
   },
   created() {
-    this._getRecommend()
-    this._getDiscList()
+    setTimeout(() => {
+      this._getRecommend()
+      this._getDiscList()
+    }, 1000)
   },
   methods: {
     _getRecommend() {
@@ -46,14 +62,20 @@ export default {
     _getDiscList() {
       getDiscList().then((res) => {
         if (res.code === ERR_OK) {
-          console.log(res.data.list)
           this.discList = res.data.list
         }
       })
+    },
+    loadImage() {
+      // 歌单图片首次加载完毕，调用scrll.refresh方法重新initScroll
+      if (!this.checkLoaded) {
+        this.$refs.scroll.refresh()
+        this.checkLoaded = true
+      }
     }
   },
   components: {
-    Slider
+    Slider, Scroll
   }
 }
 </script>
