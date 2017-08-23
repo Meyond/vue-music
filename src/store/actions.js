ebee74b692c9b17ac70305fcd2f16a852b6cdd13
meyond <332595512@qq.com>
2017-08-24 01:20:40 +0800

    歌曲播放页面

--- src/store/actions.js
+++ src/store/actions.js
@@ -1,6 +1,7 @@
 // 5.通过action操作mutation来改变数据，而不是直接操作mutation
 import * as types from './types.js'
 
+// 点选歌曲
 export function selectPlay({commit, state}, {list, index}) {
   commit(types.SET_SEQUENCE_LIST, list)
   commit(types.SET_PLAYLIST, list)
